308cd75f04baf9bdd37bcb239511553501cb9ef3
Thomas Ng <thomas.ng@link.cuhk.edu.hk>
2025-05-22 16:54:11 +0800

    Refactor powerlaw_smoothed function parameters to set default values for alpha, mmin, mmax, and delta

--- population_models/mass.py
+++ population_models/mass.py
@@ -36,7 +36,7 @@ def _powerlaw_smoothed_unnorm(m, alpha, mmax, mmin, delta):
     return powerlaw_truncated(m, alpha, mmin, mmax)*smoothing(m, mmin, delta)
     
 # @njit
-def powerlaw_smoothed(m, alpha, mmax, mmin, delta, **kwargs):
+def powerlaw_smoothed(m, alpha=3.5, mmin=5, mmax=90, delta=5, **kwargs):
     x  = np.linspace(mmin, mmax, 1000)
     dx = x[1]-x[0]
     n  = np.sum(_powerlaw_smoothed_unnorm(x, alpha, mmax, mmin, delta)*dx)
